f6e8ea9dddac0e003ccb3786afa455dc51850e94
Laura Nikulski <73386236+lauranikulski@users.noreply.github.com>
2023-10-07 17:05:47 +0900

    Add csrf protection, initalise SQLite database

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
 # libraries 
-from flask import Flask, request, jsonify, render_template, url_for, redirect
+from flask import Flask, request, jsonify, render_template, url_for, redirect, Response, abort
 from pydantic import BaseModel, SecretBytes, SecretStr, ValidationError # to define schema
 from flask_wtf import FlaskForm
+from flask_wtf.csrf import CSRFProtect
 from wtforms import StringField, SubmitField, PasswordField, RadioField, validators
 from wtforms.validators import DataRequired
 from flask_sqlalchemy import SQLAlchemy
 import sqlite3
-
-
 
 # Create Flask application instance, configuration, database 
 app = Flask(__name__)
@@ -15,6 +14,9 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = "my_secret_key"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///emails_passwords.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+csrf = CSRFProtect()
+csrf.init_app(app)
 
 db = SQLAlchemy(app)
 
@@ -36,9 +38,9 @@ def hello_root():
     return {'message': 'root'}
 
 # create email registration endpoint
-@app.route('/enter_email.html', methods=['GET', 'POST'])
+@app.route('/enter_email', methods=['GET', 'POST'])
 def enter_email():
-    loginform = LoginForm(csrf_enabled=False)
+    loginform = LoginForm(csrf_enabled=True)
     if loginform.validate_on_submit():
         if request.method == 'POST': # for each post request there should be a valid email and password
             data = request.form
@@ -51,8 +53,5 @@ def enter_email():
 
 
 # Run development server locally
-if __name__ == '__app__':
+if __name__ == '__main__':
     app.run(debug=True)
-    
-    
-    
